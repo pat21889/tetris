80a88fb77dd6717d36d4f8798c4cd0ce0b93eb2b
pat21889 <korapatphu@gmail.com>
2026-06-18 22:24:16 +0700

    feat: add rotate_matrix function into utils/helper.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,4 +1,4 @@
-from typing import TypeVar
+from typing import TypeVar, List
 
 numeric = TypeVar('numeric', int, float)
 
@@ -6,3 +6,18 @@ def clamp(value: numeric, min_val: numeric, max_val: numeric) -> numeric:
     if min_val <= value <= max_val:
         return value
     return min_val if value < min_val else max_val
+
+def rotate_matrix(matrix: List[List[int]]) -> List[List[int]]:
+    if not matrix or not matrix[0]:
+        return []
+    
+    old_rows_size = len(matrix)
+    old_cols_size = len(matrix[0])
+    
+    temp = [[0] * old_rows_size for _ in range(old_cols_size)]
+    
+    for row in range(old_rows_size):
+        for col in range(old_cols_size):
+            temp[col][old_rows_size-1-row] = matrix[row][col]
+    
+    return temp
