8d00e38f55c66c2fb6926ebe96599c114d9d9393
pat21889 <korapatphu@gmail.com>
2026-06-18 22:01:30 +0700

    chore: add type hint for clamp function in utils/helpers.py and make code cleaner

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,7 +1,8 @@
-def clamp(value, min_val, max_val) :
-    if value >= min_val and value <= max_val: 
+from typing import TypeVar
+
+numeric = TypeVar('numeric', int, float)
+
+def clamp(value: numeric, min_val: numeric, max_val: numeric) -> numeric:
+    if min_val <= value <= max_val:
         return value
-    elif value < min_val: 
-        return min_val
-    elif value > max_val: 
-        return max_val
+    return min_val if value < min_val else max_val
