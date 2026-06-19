b6dd21ddbd09e1d482c8e6ab76e7907c9293fea2
pat21889 <korapatphu@gmail.com>
2026-06-19 14:31:49 +0700

    chore: change rotate function in utils/helpers.py to use zip function

--- utils/helpers.py
+++ utils/helpers.py
@@ -8,17 +8,7 @@ def clamp(value: numeric, min_val: numeric, max_val: numeric) -> numeric:
 def rotate_matrix(matrix: List[List[int]]) -> List[List[int]]:
     if not matrix or not matrix[0]:
         return []
-    
-    old_rows_size = len(matrix)
-    old_cols_size = len(matrix[0])
-    
-    temp = [[0] * old_rows_size for _ in range(old_cols_size)]
-    
-    for row in range(old_rows_size):
-        for col in range(old_cols_size):
-            temp[col][old_rows_size-1-row] = matrix[row][col]
-    
-    return temp
+    return [list(row) for row in zip(*matrix[::-1])]
 
 def flatten(matrix: List[List[int]]) -> List[int]:
     if not matrix or not matrix[0]:
